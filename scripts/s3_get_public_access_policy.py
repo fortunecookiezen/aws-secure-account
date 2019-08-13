6b87fd9be81c98b45609cf1c78808565c84e961c
jamesp <phillips.james@gmail.com>
2019-08-12 19:57:37 -0500

    adding new scripts to undo the fucking around I'm doing

---
+++ scripts/s3_get_public_access_policy.py
@@ -0,0 +1,23 @@
+import json, sys, boto3
+
+
+def get_account_id(profile):
+    session = boto3.Session(profile_name=profile)
+    client = session.client("sts")
+    account_id = client.get_caller_identity()["Account"]
+
+    return account_id
+
+
+def print_bucket_policy(profile):
+    session = boto3.Session(profile_name=profile)
+    client = session.client("s3control", region_name="us-east-1")
+
+    response = client.get_public_access_block(AccountId=get_account_id(profile))
+    print(json.dumps(response, indent=4))
+
+
+if (
+    __name__ == "__main__"
+):  # takes profile_name as an argument. this could be done simpler, but I use profiles.
+    print_bucket_policy(sys.argv[1])
